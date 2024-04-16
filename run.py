25c81c001bc1a266f611a08ae069465dd88426a2
m13uz <mur4t.uzun@gmail.com>
2024-04-16 21:29:12 +0000

    env stuff

--- run.py
+++ run.py
@@ -1,18 +1,30 @@
+import os
 from loguru import logger
 from skyrim import Skyrim
 from dataclasses import asdict
+from dotenv import load_dotenv, dotenv_values
 
+# Load environment variables from .env file
+env_vars = dotenv_values(".env")
+
+# Print the contents of the .env file
+for key, value in env_vars.items():
+    print(f"{key}: {value}")
+
+load_dotenv()
 # NOTE:
 # ERA5 variables are mean values for previous hour,
 # i.e. 13:01 to 14:00 are labelled as "14:00"
 
+logger.info(f"CDSAPI_URL: {os.environ.get('CDSAPI_URL')}")
+logger.info(f"CDSAPI_KEY: {os.environ.get('CDSAPI_KEY')}")
 
 logger.success("imports successful")
 model = Skyrim(
     # model_name="graphcast",
-    # model_name="panguweather",
+    model_name="panguweather",
     # model_name="fourcastnetv2",
-    model_name="fourcastnet",
+    # model_name="fourcastnet",
     date="20240324",  # str YYYYMMDD
     time=12,  # int 0-23
     lead_time=12,  # hours
