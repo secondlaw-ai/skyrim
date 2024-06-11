a3bc0716a36a7f8d3a10d8c98cb7be6f58154b3c
efesurekli <e.surekli@gmail.com>
2024-06-11 23:19:08 +0100

    Add past runs from openmeteo

--- tests/libs/test_benchmark.py
+++ tests/libs/test_benchmark.py
@@ -1,4 +1,4 @@
-from skyrim.libs.benchmark.openmeteo import forecast
+from skyrim.libs.benchmark.openmeteo import forecast, forecast_past
 from skyrim.libs.benchmark.observations import observe
 from datetime import date
 
@@ -10,4 +10,17 @@ def test_fetch_and_combine_observations():
     df = observe(lat, lon, ("t2m", "wspd"), start, end)
     dff = forecast(lat, lon, ("t2m", "wspd10m"), start, end)
     res = dff.join(df)
-    assert res.shape == (72, 2)
+    assert res.shape == (72, 4)
+
+
+def test_historical_runs():
+    df = forecast_past(lat, lon, ("t2m", "wspd10m"), (1, 2), start, end)
+    assert df.shape == (72, 6)
+    assert set(df.columns) == {
+        "t2m",
+        "wspd10m",
+        "temperature_2m_previous_day1",
+        "wind_speed_10m_previous_day1",
+        "temperature_2m_previous_day2",
+        "wind_speed_10m_previous_day2",
+    }
