a15d75e45c738a55347d2f70dca260bdfe5716ad
Ubuntu <ubuntu@ip-172-31-33-152.eu-west-1.compute.internal>
2024-07-09 15:07:46 +0000

    Fix tests

--- tests/core/test_graphcast.py
+++ tests/core/test_graphcast.py
@@ -9,5 +9,3 @@ def test_output_mapping():
     # TODO: define a data container validation for prediction.
     pred, _ = model.predict("20240404", "0000", 6, save=False)
     assert set(pred.prediction.dims) == {"time", "channel", "lat", "lon"}
-    breakpoint()
-    print("done")
